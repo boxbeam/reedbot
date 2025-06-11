60c115f910003dd7a53fce14ddaf791d351fa7dd
boxbeam <instantiable@gmail.com>
2025-06-11 00:53:00 -0400

    Update command parser to use new experimental untwine syntax

--- src/command.rs
+++ src/command.rs
@@ -12,8 +12,6 @@ pub enum ParseTimeError {
     Untwine(#[from] ParserError),
     #[error("{0}")]
     ParseInt(#[from] ParseIntError),
-    #[error("Invalid weekday: {0}")]
-    InvalidWeekday(String),
 }
 
 pub enum Command {
@@ -30,49 +28,34 @@ parser! {
     [error = ParseTimeError, data = jiff::tz::TimeZone]
     num: num=<'0'-'9'+> -> u64 { num.parse()? }
 
-    delay: num=num unit=<('h'|'m'|'s'|'w'|'d')> -> u64 {
-        let multiplier = match unit {
-            "w" => 7 * 24 * 60 * 60 * 1000,
-            "d" => 24 * 60 * 60 * 1000,
-            "h" => 60 * 60 * 1000,
-            "m" => 60 * 1000,
-            "s" => 1000,
-            _ => unreachable!(),
-        };
-        num * multiplier
+    unit = match {
+        "w" => 7 * 24 * 60 * 60 * 1000,
+        "d" => 24 * 60 * 60 * 1000,
+        "h" => 60 * 60 * 1000,
+        "m" => 60 * 1000,
+        "s" => 1000,
+    } -> u64;
+
+    delay: num=num unit=unit -> u64 {
+        num * unit
     }
 
     months: num=num "mo" -> TimeModifier {
         TimeModifier::Months(num)
     }
 
-    weekday: day=<
-            "monday"
-            | "Monday"
-            | "tuesday"
-            | "Tuesday"
-            | "wednesday"
-            | "Wednesday"
-            | "thursday"
-            | "Thursday"
-            | "friday"
-            | "Friday"
-            | "saturday"
-            | "Saturday"
-            | "sunday"
-            | "Sunday"
-            >
-        -> TimeModifier {
-        TimeModifier::Weekday(match &*day.to_lowercase() {
-            "monday" => Weekday::Monday,
-            "tuesday" => Weekday::Tuesday,
-            "wednesday" => Weekday::Wednesday,
-            "thursday" => Weekday::Thursday,
-            "friday" => Weekday::Friday,
-            "saturday" => Weekday::Saturday,
-            "sunday" => Weekday::Sunday,
-            _ => return Err(ParseTimeError::InvalidWeekday(day.to_string()))
-        }.to_monday_zero_offset())
+    weekday = match {
+        ("monday" | "Monday") => Weekday::Monday,
+        ("tuesday" | "Tuesday") => Weekday::Tuesday,
+        ("wednesday" | "Wednesday") => Weekday::Wednesday,
+        ("thursday" | "Thursday") => Weekday::Thursday,
+        ("friday" | "Friday") => Weekday::Friday,
+        ("saturday" | "Saturday") => Weekday::Saturday,
+        ("sunday" | "Sunday") => Weekday::Sunday,
+    } -> Weekday;
+
+    weekday_modifier: weekday=weekday -> TimeModifier {
+        TimeModifier::Weekday(weekday.to_monday_zero_offset())
     }
 
     delays: delays=delay+ -> TimeModifier { TimeModifier::Delay(delays.into_iter().sum()) }
@@ -91,29 +74,20 @@ parser! {
         TimeModifier::Date { year, month, day }
     }
 
-    modifier = (months | delays | time_of_day | date | weekday) -> TimeModifier;
+    modifier = (months | delays | time_of_day | date | weekday_modifier) -> TimeModifier;
 
-    schedule_reminder: ("r" | "remindme" | "reminder") " " time=time ";" " "? message=<.+> -> Command {
-        Command::ScheduleReminder(time, message.to_string())
-    }
+    match_commands = match {
+        ("r" | "remindme" | "reminder") " " time=time ";" " "? message=<.+> => Command::ScheduleReminder(time, message.to_string()),
+        ("h" | "help") => Command::Help,
+        ("setinterval" | "si") " " id=num " " modifiers=modifier$" "+ => Command::SetInterval(id, modifiers),
+        ("clearinterval" | "ci") " " id=num => Command::ClearInterval(id),
+        ("cancelreminder" | "cr") " " id=num => Command::CancelReminder(id),
+        ("reminders" | "rs") => Command::ListReminders,
+        ("r" | "remindme" | "reminder") " " time=time ";" " "? message=<.+> => Command::ScheduleReminder(time, message.to_string()),
+        ("tz" | "timezone") " " timezone=<.+> => Command::SetTimezone(timezone.to_string())
+    } -> Command;
 
-    cancel_reminder: ("cancelreminder" | "cr") " " id=num -> Command { Command::CancelReminder(id) }
-
-    set_interval: ("setinterval" | "si") " " id=num " " modifiers=modifier$" "+ -> Command {
-        Command::SetInterval(id, modifiers)
-    }
-
-    clear_interval: ("clearinterval" | "ci") " " id=num -> Command { Command::ClearInterval(id) }
-
-    list_reminders: ("reminders" | "rs") -> Command { Command::ListReminders }
-
-    set_timezone: ("tz" | "timezone") " " timezone=<.+> -> Command {
-        Command::SetTimezone(timezone.to_string())
-    }
-
-    help: ("h" | "help") -> Command { Command::Help }
-
-    pub command = "$" (schedule_reminder | set_interval | clear_interval | list_reminders | help | cancel_reminder | set_timezone) -> Command;
+    pub command = "$" match_commands -> Command;
 
     pub time: modifiers=modifier$" "+ -> Zoned {
         let mut date = Zoned::now().with_time_zone(__ctx.data().clone());
